080d36adf41436a29addbd93176e5df07ec04feb
Krisztian Kakasi <cristy11_1998@yahoo.com>
2019-09-09 20:03:33 +0300

    studied method and constructor overloading

--- src/main/java/org/fasttrackit/AutoVehicle.java
+++ src/main/java/org/fasttrackit/AutoVehicle.java
@@ -13,6 +13,12 @@ public class AutoVehicle extends Vehicle {
     public AutoVehicle(Engine engine) {
         this.engine = engine;
     }
+    //constructor overloading
+    public AutoVehicle() {
+        this.engine = new Engine();
 
+        //this(new Engine()); - acelasi efect ca linia de mai sus
+
+    }
 
 }
