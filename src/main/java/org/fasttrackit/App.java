d98cba1851d77a60ae35be2650f6f324de867966
Krisztian Kakasi <cristy11_1998@yahoo.com>
2019-09-09 19:50:10 +0300

    added encapsulation for Vehicle class; studied static variables

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -14,17 +14,17 @@ public class App {
 
 
         Car car = new Car(engine);
-        car.name = "Dacia";
-        car.fuelLevel = 60;
-        car.mileage = 8.5;
+        car.setName ("Dacia");
+        car.setFuelLevel(60);
+        car.setMileage(8.5);
         car.doorCount = 5;
-        car.maxSpeed = 180;
-        car.fuelType = "Gasoline";
+        car.setMaxSpeed(180);
+        car.setFuelType("Gasoline");
 
 
-        System.out.println(car.name);
-        System.out.println(car.traveledDistance);
-        System.out.println(car.running);
+        System.out.println(car.getName());
+        System.out.println(car.getTraveledDistance());
+        System.out.println(car.isRunning());
 
         car.accelerate(60, 1);
 
@@ -35,22 +35,22 @@ public class App {
 
 
         Car car2 = new Car(engine2);
-        car2.name = "Golf";
-        car2.fuelLevel = 55;
-        car2.mileage = 7.5;
+        car2.setName("Golf");
+        car2.setFuelLevel(55);
+        car2.setMileage(7.5);
+        car2.setMaxSpeed(195);
+        car2.setFuelType("Diesel");
         car2.doorCount = 3;
-        car2.maxSpeed = 195;
-        car2.fuelType = "Diesel";
 
-        car.name = "Skoda";
+        car.setName("Skoda");
 
 //        car.name = null;
 
         //concatenare = lipire cu operatorul +
-        System.out.println("Car2 name: " + car2.name);
-        System.out.println("Car1 name: " + car.name);
+        System.out.println("Car2 name: " + car2.getName());
+        System.out.println("Car1 name: " + car.getName());
 
-        car = car2;
+       // car = car2;
         System.out.println("Modulo examples:");
         System.out.println(4 % 2);
         System.out.println(4 % 3);
@@ -59,6 +59,22 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
         System.out.println("Imput from user: " + text);
+
+//        System.out.println("Static variables example:");
+//        //we should only call static variables from the class itself (Car),
+//        //not from a variable containing an objectof that class
+//
+//        car.totalCount = 1;
+//
+//        System.out.println("Total count in car: " + car.totalCount);
+//
+//        car2.totalCount = 12;
+//        System.out.println("Total count in car after setting car2: " + car.totalCount);
+//        System.out.println("Total count in car2: " + car2.totalCount);
+//        System.out.println("Total count in Car class: " + Car.totalCount);
+
+
+
 
 
     }
--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -2,13 +2,21 @@ package org.fasttrackit;
 
 
 public class Vehicle {
-    String name;
-    double mileage;
-    double maxSpeed;
-    String fuelType;
-    double fuelLevel;
-    double traveledDistance;
-    boolean running;
+    //Denumire constante = DENUMIRE_CONSTANTA
+
+    private static int totalCount;
+
+    private String name;
+    private double mileage;
+    private double maxSpeed;
+    private String fuelType;
+    private double fuelLevel;
+    private double traveledDistance;
+    private boolean running;
+
+    public Vehicle() {
+        totalCount++;
+    }
 
     public double accelerate(double speed, double durationInHours) {
         System.out.println(name + " is accelerating with " + speed + " km/h for " + durationInHours + " h. ");
@@ -30,5 +38,67 @@ public class Vehicle {
 
 
         return distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(double mileage) {
+        this.mileage = mileage;
+    }
+
+    public double getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(double maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public double getFuelLevel() {
+        return fuelLevel;
+    }
+
+    public void setFuelLevel(double fuelLevel) {
+        this.fuelLevel = fuelLevel;
+    }
+
+    public double getTraveledDistance() {
+        return traveledDistance;
+    }
+
+    public void setTraveledDistance(double traveledDistance) {
+        this.traveledDistance = traveledDistance;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public static int getTotalCount() {
+        return totalCount;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+
+
     }
 }
